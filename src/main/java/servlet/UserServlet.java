ec219d1091bb2d41c3895a8e3a37492574afbba5
Viktor Sazhin <sa.vitek98@gmail.com>
2022-02-12 20:05:33 +0300

    fix bag in UserServlet and index.jsp

--- src/main/java/servlet/UserServlet.java
+++ src/main/java/servlet/UserServlet.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-@WebServlet("/")
+@WebServlet("/tomcattest")
 public class UserServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private UserDao userDAO;
